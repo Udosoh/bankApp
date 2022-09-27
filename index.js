68c3f1a1689e18f68aee655afa634aa87a12eb11
Udosoh <105041343+Udosoh@users.noreply.github.com>
2022-09-27 12:57:19 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,64 @@
 const account1 = {
   owner: "Iniodu Udosoh",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  password: 2222,
+  movements: [600, 1400, -150, -790, -3210, -1000, 8500, -30],
+  password: 1111,
+  recipent: [
+    "Otobong",
+    "Daddy",
+    "Sweet heart",
+    "Mumy Joy",
+    "Poseidon",
+    "Ediomo",
+    "Erinma love",
+    "Crush",
+  ],
 };
 const account2 = {
   owner: "Nsisong Udosoh",
   movements: [50000, 700, -150, -9090, -3210, -76000, 85000, -30],
   password: 2222,
+  recipent: [
+    "Samuel",
+    "Glory",
+    "Sweet heart",
+    "DSTV guy",
+    "Dickson",
+    "Grand Ma",
+    "Erinma Blessing",
+    "Land Lord",
+  ],
 };
 
 const account3 = {
   owner: "Ekpono Maurice",
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   password: 3333,
+  recipent: [
+    "Dorathy",
+    "Anita",
+    "Pretty",
+    "LOML",
+    "Sugar Mummy",
+    "DEar Sis",
+    "Emmedion Obot",
+    "Iniodu Udosoh",
+  ],
 };
 
 const account4 = {
-  owner: "Emmedion Immanuel",
-  movements: [430, 1000, 700, 50, 90],
+  owner: "Emmediong Immanuel",
+  movements: [-430, -1000, -700, 5000000, -90, 6 - 00, -5000, 8000],
   password: 4444,
+  recipent: [
+    "Nsisong",
+    "Daddy",
+    "ife",
+    "Dear Mum",
+    "Ifreke",
+    "Uduak enyoung",
+    "Colleauge",
+    "Emma Love",
+  ],
 };
 const accounts = [account1, account2, account3, account4];
 
@@ -29,6 +69,11 @@ let summary = document.querySelector(".summary");
 let balances = document.querySelector(".number");
 let apppage = document.querySelector(".con");
 let loginpage = document.querySelector(".login");
+let transfername = document.querySelector(".accountname");
+let transferno = document.querySelector(".ammountno");
+let send = document.querySelector(".sendbutton");
+let welcomebk = document.querySelector(".acc");
+let switchacc = document.querySelector(".switch");
 
 const creatUsername = function (accounts) {
   accounts.forEach((acc) => {
@@ -37,9 +82,14 @@ const creatUsername = function (accounts) {
 };
 creatUsername(accounts);
 
-const displaytransactions = function (movement) {
+const displaytransactions = function (acco) {
   summary.innerHTML = "";
-  movement.forEach(function (val, i) {
+
+  let dave = acco.recipent.map(function (val, i) {
+    return val;
+  });
+
+  acco.movements.forEach(function (val, i) {
     let type = val > 0 ? "deposit from" : "transfer to";
     let str = `<div class="transactions">
             <div class="date">
@@ -53,7 +103,7 @@ const displaytransactions = function (movement) {
                 <div class="account">
                   <div class="owner">Iniodu Udosoh</div>
                   <div class="transactiontype">
-                    a ${type} "INIODU UDOSOH"
+                    a ${type} <span class="namess" >${dave[i]}</span>
                   </div>
                 </div>
               </div>
@@ -65,24 +115,53 @@ const displaytransactions = function (movement) {
   });
 };
 
-const calculatebalance = function (movement) {
-  let balance = movement.reduce((acc, val, i) => acc + val, 0);
-  balances.innerHTML = `${balance} N`;
+let balance;
+
+const calculatebalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, val, i) => acc + val, 0);
+  balances.innerHTML = `${acc.balance} N`;
 };
 
 let currentacount;
 
 submitLogin.addEventListener("click", function (e) {
   e.preventDefault();
-
   currentacount = accounts.find((acc) => acc.username === usernamelogin.value);
+
   if (
     currentacount?.username === usernamelogin.value &&
     currentacount.password === Number(passwordlogin.value)
   ) {
     apppage.classList.remove("val");
     loginpage.classList.add("val");
-    calculatebalance(currentacount.movements);
-    displaytransactions(currentacount.movements);
+    usernamelogin.value = "";
+    passwordlogin.value = "";
+    calculatebalance(currentacount);
+    displaytransactions(currentacount);
+    welcomebk.innerHTML = `welcome back ${currentacount.owner}`;
   }
+});
+
+send.addEventListener("click", function (e) {
+  e.preventDefault();
+  reciver = accounts.find((val) => val.username === transfername.value);
+  let ammount = Number(transferno.value);
+
+  console.log(reciver);
+
+  if (reciver && ammount > 0 && currentacount.balance >= ammount) {
+    reciver.movements.push(ammount);
+    currentacount.movements.push(-ammount);
+    currentacount.recipent.push(transfername.value);
+    reciver.recipent.push(currentacount.username);
+    calculatebalance(currentacount);
+    displaytransactions(currentacount);
+    transfername.value = "";
+    transferno.value = "";
+  }
+});
+
+switchacc.addEventListener("click", function (e) {
+  apppage.classList.add("val");
+  loginpage.classList.remove("val");
 });
